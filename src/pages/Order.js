cfcce3b1bdd7999f21b2ce380d76b78c3d1dc918
dieg0espx <espinosa9mx@gmail.com>
2024-02-23 13:43:35 -0800

    .

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -5,12 +5,14 @@ import { getFirestore } from 'firebase/firestore';
 import { collection, getDocs, getDoc, deleteDoc } from 'firebase/firestore';
 import { doc } from "firebase/firestore";
 import {app} from '../Firebase';
+import { load } from 'cheerio';
 
 function Order() {
     const db = getFirestore(app)
     const { id } = useParams();
     const [order, setOrder] = useState([])
-    
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [showLoader, setShowLoader] = useState(true);
 
     async function getBooking(id){
         const docRef = doc(db, "bookings", id);
@@ -51,18 +53,32 @@ function Order() {
       getLocation();
     }, []);
 
+    
 
+    useEffect(() => {
+      if(isLoaded == true){
+        setTimeout(() => {
+          setShowLoader(false)
+        }, 1000); // 1000 milliseconds = 1 second
+      }
+    }, [isLoaded]); // Empty dependency array ensures the effect runs only once
+  
 
   return (
     <div className='order'>
         <Header />
-        <iframe src={`https://ttfconstruction.com/MapsAPI/index.php?&from=${latitude},${longitude}&&to=${order.address}`} />
-        <div className='details'>
-            <p id="name"> {order.name} {order.lastName}</p>
-            <p id="inflatable"> {order.inflatableName}</p>
-            <p id="address"> {order.address} </p>
-            <button onClick={()=>window.location.href `tel:${order.phone}`}> Call Now </button>
+        <div className='wrapper-loader' style={{display: showLoader ? "flex":"none"}}>
+          <span className='loader'></span>
+          <p id='legend'> Fetching Data</p>
         </div>
+        
+          <iframe onLoad={()=>setIsLoaded(true)} src={`https://ttfconstruction.com/MapsAPI/index.php?&from=${latitude},${longitude}&&to=${order.address}`} />
+          <div className='details'>
+              <p id="name"> {order.name} {order.lastName}</p>
+              <p id="inflatable"> {order.inflatableName}</p>
+              <p id="address"> {order.address} </p>
+              <button onClick={()=>window.location.href `tel:${order.phone}`}> Call Now </button>
+          </div>
     </div>
   )
 }
